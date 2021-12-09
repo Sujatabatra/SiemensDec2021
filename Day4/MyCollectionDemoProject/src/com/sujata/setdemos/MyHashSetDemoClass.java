9ae600da36b5cdf5fafd02b64a19652f8f4d6092
Sujatabatra <sujatabatra@hotmail.com>
2021-12-09 11:09:30 +0530

    Add files via upload

---
+++ Day4/MyCollectionDemoProject/src/com/sujata/setdemos/MyHashSetDemoClass.java
@@ -0,0 +1,45 @@
+package com.sujata.setdemos;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MyHashSetDemoClass {
+
+	public static void main(String[] args) {
+		/*
+		 * HashSet is the unordered unique collection of objects
+		 * and collection is auto growable and auto shrinkable
+		 */
+		Set<Integer> collection=new HashSet<Integer>();
+		
+		System.out.println("Size of my Collection : "+collection.size());
+		System.out.println(collection);
+		
+		collection.add(new Integer(10));
+		collection.add(new Integer(30));
+		collection.add(new Integer(90));
+		collection.add(new Integer(20));
+		
+		System.out.println("Size of my Collection : "+collection.size());
+		System.out.println(collection);
+		
+		collection.remove(30);
+
+		System.out.println("Size of my Collection : "+collection.size());
+		System.out.println(collection);
+		
+		System.out.println("Traversal using for each loop");
+		for(Integer element:collection) {
+			System.out.println(element);
+		}
+		
+		System.out.println("Traversal using Iterator Interface");
+		Iterator<Integer> iterator=collection.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+	}
+
+}
