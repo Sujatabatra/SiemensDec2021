44acf9bba5c8baac0384e95556ea1d4162ea27b2
Sujatabatra <sujatabatra@hotmail.com>
2021-12-10 10:56:33 +0530

    Add files via upload

---
+++ Day5/MyFirstJunitDemoProject/src/com/sujata/demo/Calculator.java
@@ -0,0 +1,14 @@
+package com.sujata.demo;
+
+public class Calculator {
+
+	//should divide positive integers
+	public int divide(int number1,int number2) {
+		return -1;
+	}
+	
+	//should add positive intergers
+	public int add(int number1,int number2) {
+		return -1;
+	}
+}
