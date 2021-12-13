3570a0e02ba1a0e06aa9158f1b2f66f7a1cd5403
Sujatabatra <sujatabatra@hotmail.com>
2021-12-13 11:05:47 +0530

    Add files via upload

---
+++ Day6/MyFirstSpringGreetProject/src/com/sujata/consumer/GreetConsumer.java
@@ -0,0 +1,25 @@
+package com.sujata.consumer;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sujata.producer.GoodMorning;
+import com.sujata.producer.Greet;
+
+public class GreetConsumer {
+	
+	public static void main(String args[]) {
+//		Greet greet=new GoodMorning();
+		
+		/*
+		 * life cycle ( creation of objects) is being done by spring container i.e ApplicationContext
+		 * by considering configuration file , which is .xml file stored in classpath i.e erc folder
+		 */
+		ApplicationContext springContainer=new ClassPathXmlApplicationContext("greet.xml");
+		
+		Greet greet1=(Greet)springContainer.getBean("eve");
+		
+		greet1.wish("Sujata");
+	}
+
+}
