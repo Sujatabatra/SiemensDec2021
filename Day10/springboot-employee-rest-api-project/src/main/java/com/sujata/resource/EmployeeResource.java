47066c2bc3bdc5464d77c6dd530bc0325736fd7a
Sujatabatra <sujatabatra@hotmail.com>
2021-12-17 15:04:30 +0530

    Add files via upload

--- Day10/springboot-employee-rest-api-project/src/main/java/com/sujata/resource/EmployeeResource.java
+++ Day10/springboot-employee-rest-api-project/src/main/java/com/sujata/resource/EmployeeResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sujata.bean.Employee;
@@ -25,5 +27,14 @@ public class EmployeeResource {
 	@GetMapping(path="/employees/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
 	public Employee getEmployeeByIdResource(@PathVariable("id") int employeeId) {
 		return employeeService.searchEmployeeById(employeeId);
+	}
+	
+	@PostMapping(path = "/employees",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Employee saveEmployeeResource(@RequestBody Employee employee) {
+		boolean status=employeeService.insertEmployee(employee);
+		if(status)
+			return employee;
+		else 
+			return new Employee();
 	}
 }
