b11c3c7a28bd216e0dac0fff3dea6d2c4f61a662
Sujatabatra <sujatabatra@hotmail.com>
2021-12-17 15:24:42 +0530

    Add files via upload

--- Day10/springboot-employee-rest-api-project/src/main/java/com/sujata/resource/EmployeeResource.java
+++ Day10/springboot-employee-rest-api-project/src/main/java/com/sujata/resource/EmployeeResource.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,26 +17,49 @@ import com.sujata.service.EmployeeService;
 
 @RestController
 public class EmployeeResource {
-	
+
 	@Autowired
 	private EmployeeService employeeService;
 
 	@GetMapping(path = "/employees", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Employee> getAllEmployeesResource(){
+	public List<Employee> getAllEmployeesResource() {
 		return employeeService.getEmployeeList();
 	}
-	
-	@GetMapping(path="/employees/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@GetMapping(path = "/employees/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Employee getEmployeeByIdResource(@PathVariable("id") int employeeId) {
 		return employeeService.searchEmployeeById(employeeId);
 	}
-	
-	@PostMapping(path = "/employees",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+
+	@PostMapping(path = "/employees", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Employee saveEmployeeResource(@RequestBody Employee employee) {
-		boolean status=employeeService.insertEmployee(employee);
-		if(status)
+		boolean status = employeeService.insertEmployee(employee);
+		if (status)
 			return employee;
-		else 
+		else
 			return new Employee();
+	}
+
+	@DeleteMapping(path="/employees/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public Employee deleteEmployeeResource(@PathVariable int id) {
+		Employee employee = employeeService.searchEmployeeById(id);
+		if (employee != null) {
+			boolean status = employeeService.removeEmployee(id);
+			if (status)
+				return employee;
+		}
+		return new Employee();
+
+	}
+	
+	
+	@PutMapping(path = "employees/{id}/{salary}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public Employee updateEmployeeSalaryResource(@PathVariable("id") int id,@PathVariable("salary") int salary) {
+		boolean status=employeeService.updateEmployee(id, salary);
+		if(status) {
+			Employee employee=employeeService.searchEmployeeById(id);
+			return employee;
+		}
+		return new Employee();
 	}
 }
